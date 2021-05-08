528920c545a7fd90467f5f0d6fa5878d3dd4b88b
chris <cseiter@gmail.com>
2021-05-08 16:14:59 -0400

    Evergreen class created and tests passed

--- index.js
+++ index.js
@@ -56,3 +56,13 @@ class Pet {
         return (super.definition() + " Deciduous trees shed their leaves annually.")
     }
 }
+
+class Evergreen extends Tree {
+    constructor(species,name) {
+        super(species);
+        this.name = name;
+    }
+  static definition() {
+      return (super.definition() + " Evergreens keep their leaves all year round.")
+  }
+}
